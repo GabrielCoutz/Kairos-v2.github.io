7e50789f14b6d9e62022eae727088be1f0a3d424
Gabriel Coutinho <gabriel.coutinho.cassiano@gmail.com>
2022-09-01 10:06:39 -0300

    Atualizações

--- index.php
+++ index.php
@@ -23,7 +23,7 @@
         session_unset();
         session_destroy();
         session_write_close();
-        setcookie(session_name(),'',0,'/', NULL, NULL, true, true);
+        setcookie(session_name(),'',0,'/', NULL, NULL, true);
         session_regenerate_id(true);
     ?>
 </head>
